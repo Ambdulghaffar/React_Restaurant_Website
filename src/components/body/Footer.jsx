191af90c5b702c1808626e2bf690111e5ba49a45
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-04-01 23:28:52 +0000

    Last commit

--- src/components/body/Footer.jsx
+++ src/components/body/Footer.jsx
@@ -2,25 +2,26 @@ import React, { useState } from "react";
 import { FaPhone } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import { GiPositionMarker } from "react-icons/gi";
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaSquareInstagram } from "react-icons/fa6";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-function Hoverlink({to,children}) {
+function Hoverlink({ href, children }) {
   const [hover, setHover] = useState(false);
   return (
-    <span
-      to={to}
-      className={`text-decoration-none ${hover ? "text-warning" : "text-white"}`}
+    <Link
+      href={href}
+      className={`text-decoration-none ${
+        hover ? "text-warning" : "text-white"
+      }`}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
       {children}
-    </span>
+    </Link>
   );
 }
-
 
 function Footer() {
   return (
@@ -31,60 +32,93 @@ function Footer() {
             <p className="fs-3 fw-bold">Ce qu'on offre</p>
             <div className="d-flex">
               <div className="me-5">
-              <p><Hoverlink to="">Pizzas</Hoverlink></p>                
-              <p><Hoverlink to="">Salade</Hoverlink></p>                
-              <p><Hoverlink to="">Burgers</Hoverlink></p>                
+                <p>
+                  <Hoverlink to="/menu">Pizzas</Hoverlink>
+                </p>
+                <p>
+                  <Hoverlink to="/menu">Salade</Hoverlink>
+                </p>
+                <p>
+                  <Hoverlink to="/menu">Burgers</Hoverlink>
+                </p>
               </div>
               <div className="">
-              <p><Hoverlink to="">Desserts</Hoverlink></p> 
-              <p><Hoverlink to="">Drinks</Hoverlink></p> 
-              <p><Hoverlink to="">Tacos</Hoverlink></p> 
+                <p>
+                  <Hoverlink to="/menu">Desserts</Hoverlink>
+                </p>
+                <p>
+                  <Hoverlink to="/menu">Drinks</Hoverlink>
+                </p>
+                <p>
+                  <Hoverlink to="/menu">Tacos</Hoverlink>
+                </p>
               </div>
             </div>
           </div>
           <div className="col my-5">
             <p className="fs-3 fw-bold">Liens</p>
             <div>
-            <p><Hoverlink to="/">Acceuil</Hoverlink></p>
-            <p><Hoverlink to="/menu">Menu</Hoverlink></p>
-            <p><Hoverlink to="/commandes">Commandes</Hoverlink></p>
+              <p>
+                <Hoverlink to="/">Acceuil</Hoverlink>
+              </p>
+              <p>
+                <Hoverlink to="/menu">Menu</Hoverlink>
+              </p>
+              <p>
+                <Hoverlink to="/commandes">Contacts</Hoverlink>
+              </p>
             </div>
           </div>
           <div className="col my-5">
             <p className="fs-3 fw-bold">Contacts</p>
             <p>
               <FaPhone className="me-4 fs-5" />
-              <Link to="" className="text-decoration-none text-white">
+              <a href="" className="text-decoration-none text-white">
                 <Hoverlink>0647890345</Hoverlink>
-              </Link>
+              </a>
             </p>
             <p>
               <MdEmail className="me-4 fs-5" />
-              <Link to="#" className="text-decoration-none text-white">
+              <a href="#" className="text-decoration-none text-white">
                 <Hoverlink>pizzahouse@email.com</Hoverlink>
-              </Link>
+              </a>
             </p>
             <p>
               <GiPositionMarker className="me-4 fs-5" />
-              <Link to="" className="text-decoration-none text-white"><Hoverlink>75, bd Ibn Tachfine</Hoverlink></Link>
+              <a href="" className="text-decoration-none text-white">
+                <Hoverlink>75, bd Ibn Tachfine</Hoverlink>
+              </a>
             </p>
           </div>
-          <p className="text-center mb-5">
-            <span>© 2025. All Rights Reserved. Design by PizzaHouse</span>
-            <Link to="">
-              <FaFacebook className="mx-4 text-white  fs-4" />
-            </Link>
-            <Link to="">
-              <FaXTwitter className="me-4 text-white   fs-4" />
-            </Link>
-            <Link to="">
-              <FaSquareInstagram className="text-white  fs-4" />
-            </Link>
-          </p>
+          <div className="text-center mb-5 d-flex justify-content-center align-items-center">
+            <div>
+              <span>© 2025. All Rights Reserved. Design by PizzaHouse</span>
+            </div>
+            <div className="d-flex justify-content-center align-items-center">
+              <HoverIcon Icon={<FaFacebook />} />
+              <HoverIcon Icon={<FaXTwitter />} />
+              <HoverIcon Icon={<FaSquareInstagram />} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
+
+  function HoverIcon({ Icon }) {
+    const [hoverIcon, setHoverIcon] = useState(false);
+    return (
+      <a href="">
+        <p
+          className={`ms-4 fs-4 ${hoverIcon ? "text-info" : "text-white"}`}
+          onMouseEnter={() => setHoverIcon(true)}
+          onMouseLeave={() => setHoverIcon(false)}
+        >
+          {Icon}
+        </p>
+      </a>
+    );
+  }
 }
 
 export default Footer;
--- src/components/body/Header.jsx
+++ src/components/body/Header.jsx
@@ -4,21 +4,21 @@ import {Link} from 'react-router-dom'
 function Header() {
   return (
     <>
-<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-  <div class="container-fluid">
-    <Link class="navbar-brand" to="/">PizzaHouse</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+  <div className="container-fluid">
+    <Link className="navbar-brand  fw-bold" to="/">PizzaHouse</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
-        <li class="nav-item pe-4 fs-5">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
+        <li className="nav-item pe-4 fs-5">
           <Link className="nav-link active fs-5 fw-bold" aria-current="page" to="/">Accueil</Link>
         </li>
-        <li class="nav-item pe-4 fs-5">
+        <li className="nav-item pe-4 fs-5">
           <Link className="nav-link fs-5 fw-bold" to="/menu">Menu</Link>
         </li>
-        <li class="nav-item pe-4 fs-5">
+        <li className="nav-item pe-4 fs-5">
           <Link className="nav-link fs-5 fw-bold" to="/commandes">Commandes</Link>
         </li>
       </ul>
